773136a44a3156204296601dac9c19839986f76d
Jakeas <JakeASierra@gmail.com>
2021-02-11 18:01:38 -0500

    finished MVP

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -1,8 +1,16 @@
+import axios from 'axios'
 /*
   STEP 1: using axios, send a GET request to the following URL
     (replacing the placeholder with your Github name):
     https://api.github.com/users/<your name>
 */
+
+
+
+
+
+
+
 
 /*
   STEP 2: Inspect and study the data coming back, this is YOUR
@@ -28,7 +36,12 @@
     user, and adding that card to the DOM.
 */
 
-const followersArray = [];
+const followersArray = [  
+  'tetondan',
+  'dustinmyers',
+  'justsml',
+  'luishrd',
+  'bigknell'];
 
 /*
   STEP 3: Create a function that accepts a single object as its only argument.
@@ -49,6 +62,71 @@ const followersArray = [];
       </div>
     </div>
 */
+
+
+function userInfo (obj) {
+  const card = document.createElement('div')
+  const img = document.createElement('img')
+  const cardInfo = document.createElement('div')
+  const name = document.createElement('h3')
+  const login = document.createElement('p')
+  const location = document.createElement('p')
+  const profile = document.createElement('p')
+  const address = document.createElement('a')
+  const followers = document.createElement('p')
+  const following = document.createElement('p')
+  const bio = document.createElement('p')
+  
+  card.appendChild(img)
+  card.appendChild(cardInfo)
+  cardInfo.appendChild(name)
+  cardInfo.appendChild(login)
+  cardInfo.appendChild(location)
+  cardInfo.appendChild(profile)
+  cardInfo.appendChild(followers)
+  cardInfo.appendChild(following)
+  cardInfo.appendChild(bio)
+  profile.appendChild(address)
+
+  img.src = obj.avatar_url
+  name.textContent =obj.name
+  login.textContent = obj.login
+  location.textContent = obj.location
+  address.textContent = obj.html_url
+  followers.textContent = obj.followers
+  following.textContent = obj.following
+  bio.textContent = obj.bio
+
+  card.classList.add('card')
+  cardInfo.classList.add('card-info')
+  name.classList.add('name')
+  login.classList.add('username')
+
+
+
+  return card
+}
+
+followersArray.forEach(follower => {
+     
+  axios.get(`https://api.github.com/users/${follower}`)
+    .then((res) => {
+      console.log(res.data)
+       const cards = document.querySelector('.cards') 
+        cards.appendChild(userInfo(res.data))
+      })
+  
+    .catch((err) => {
+      console.log(err)
+    })
+  })
+
+// followersArray.forEach((follower) => {
+//   const cards = document.querySelector('.cards')
+//   const gitCard = userInfo(follower)
+//   cards.appendChild(gitCard)
+// })
+
 
 /*
   List of LS Instructors Github username's:
